625c945887b8cceac258431d04cb744ef5b0a9a3
lelemita <nywfox@gmail.com>
2022-01-17 00:41:11 +0900

    03_oxygen

--- 03_binary_diagnostic/main.cpp
+++ 03_binary_diagnostic/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int binToDecimal(string line)
@@ -53,50 +53,63 @@ int part01(const char* path) {
     return gamma * epsilon;
 }
 
-int part02(const char* path) {
+int part02(const char* path, int len) {
     ifstream ifs(path, ios::in);
     if (ifs.fail()) {
         cout << "input file error" << endl;
         return -1;
     }
 
+    string lines[len];
     string line;
-    list<string> lines;
-    while (getline(ifs, line)) {
-        lines.push_back(line);
+    int i = 0;
+    while(getline(ifs, line)) {
+        lines[i++] = line;
     }
-    ifs.close();
 
-    string oxygen;
-    string carbonDioxide;
+    int begin = 0;
+    int end = len;
+    int jdx = 0;
+    int count = 0;
+    while (count++ < 7) {
+        int middle = (begin + end) / 2;
+        sort(&lines[begin], &lines[end]);  
+        // cout << "------- " << jdx << " : " << begin << "-" << end << endl;
+        // for (int k = begin; k < end; k++) 
+        // {
+        //     cout << k << " : " << lines[k] << endl;
+        // }
+        int save = begin;
+        // while (lines[begin++][jdx] == '0') {}
+        while (true) {
+            char ch = lines[begin][jdx];
+            if (ch == '1') {
+                break;
+            }
+            begin += 1;
+        }
 
-    int idx = 0;
-    while (lines.size() > 1) {
-        int sum = 0;
-        list<int> onesIdx;
-        list<int> sIdx;
-        for (auto l : lines)
-        {
-            sum += (int)line[idx] - '0';
+        if (begin <= middle) {
+            // cout << " 대세는 1" << endl;
+            // begin -= 1;
+        } else {
+            // cout << " Win Zero " << begin << " : " << middle << endl;
+            end = begin;
+            begin = save;
         }
-        int target = 0;
-        if (sum >= lines.size()/2) {
-            target = 1;
+        if (end - begin == 0) {
+            break;
         }
-        
-        
-        idx = idx >= line.length() ? 0 : idx+1;
+        jdx = (jdx == line.length() -1)? 0 : ++jdx;
     }
-    
-
-    return binToDecimal(oxygen) * binToDecimal(carbonDioxide);
+    return binToDecimal(lines[begin]);
 }
 
 int main(void)
 {
     const char* course1 = "../example";
     cout << part01(course1) << endl;
-    cout << part02(course1) << endl;
+    cout << part02(course1, 12) << endl;
 
     // const char* course2 = "../input";
     // cout << part01(course2) << endl;
