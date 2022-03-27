dd656ce3b1324f365ef6c397fc6bc994f3bd8750
lelemita <nywfox@gmail.com>
2022-03-27 14:11:02 +0900

    17_part01

--- 17_trick-shot/main.go
+++ 17_trick-shot/main.go
@@ -1,10 +1,24 @@
+// 미적으로 풀면 안됨..40.5.....디지털 세계임..ㅠㅠ
+// https://gist.github.com/mdarrik/72835482b47e9b3e2827faa5789f8e6a
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var example = "target area: x=20..30, y=-10..-5"
 var puzzle = "target area: x=85..145, y=-163..-108"
 
+func part01(y1, y2 int) {
+	miny := -y1
+	if y1 > y2 {
+		miny = -y2
+	}
+	result := miny * (miny - 1) / 2
+	fmt.Println(result)
+}
+
 func main() {
-	fmt.Println("17")
+	part01(-10, -5)
+	part01(-163, -108)
 }
