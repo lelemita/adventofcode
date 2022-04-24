e079a8d3269093f8ccd2b998e387b49a45195d43
lelemita <nywfox@gmail.com>
2022-04-25 00:04:44 +0900

    Day 21: Dirac Dice

--- 21_dirac-dice/part02.go
+++ 21_dirac-dice/part02.go
@@ -1,89 +1,87 @@
+// struct, cache 참고: https://github.com/pemoreau/advent-of-code/blob/main/go/2021/21/day21.go
+// 재미있는 oop 풀이: https://github.com/CodeHex/advent-of-code-2021/blob/main/day21/main.go
+// 의문) 배열도 키가 되고, 3개 합으로 계산해도 되는거면... 처음 풀이는 어디가 틀렸던 거지.....ㅠㅠ
+
 package main
 
 import "fmt"
 
 type state struct {
-	player         int
-	dice           int
-	score0, score1 int
-	loc0, loc1     int
+	player int
+	dice   int
+	score  [2]int
+	loc    [2]int
 }
 
-type win struct {
-	win0 int
-	win1 int
-}
-
-var cache map[state]win
+var cache map[state][2]int
 var nums map[int]int
 
 const winScore = 21
 
-func play(s state) win {
+func (s *state) move() {
+	i := s.player
+	s.loc[i] = (s.loc[i] + s.dice) % 10
+	if s.loc[i] == 0 {
+		s.score[i] += 10
+	} else {
+		s.score[i] += s.loc[i]
+	}
+}
+
+func (s *state) changePlayer() {
+	if s.player == 0 {
+		s.player = 1
+	} else {
+		s.player = 0
+	}
+}
+
+func play(s state) [2]int {
 	if val, isExist := cache[s]; isExist {
 		return val
 
 	}
 	initS := s
-	switch s.player {
-	case 0:
-		s.loc0, s.score0 = getScore(s.loc0, s.score0, s.dice)
-		s.player = 1
-		if s.score0 >= winScore {
-			cache[initS] = win{win0: 1, win1: 0}
-			return win{win0: 1, win1: 0}
-		}
-	case 1:
-		s.loc1, s.score1 = getScore(s.loc1, s.score1, s.dice)
-		s.player = 0
-		if s.score1 >= winScore {
-			cache[initS] = win{win0: 0, win1: 1}
-			return win{win0: 0, win1: 1}
-		}
-	}
 
-	var w0, w1 int
+	s.move()
+	if s.score[s.player] >= winScore {
+		win := [2]int{0, 0}
+		win[s.player] = 1
+		cache[initS] = win
+		return win
+	}
+	s.changePlayer()
+
+	win := [2]int{0, 0}
 	for n, cnt := range nums {
 		s.dice = n
 		res := play(s)
-		w0 += res.win0 * cnt
-		w1 += res.win1 * cnt
+		win[0] += res[0] * cnt
+		win[1] += res[1] * cnt
 	}
 
-	cache[initS] = win{win0: w0, win1: w1}
-	return win{win0: w0, win1: w1}
+	cache[initS] = win
+	return win
 }
 
-func getScore(loc, score, dice int) (int, int) {
-	loc = (loc + dice) % 10
-	if loc == 0 {
-		score += 10
-	} else {
-		score += loc
-	}
-	return loc, score
-}
-
-func part2(start0, start1 int) {
+func part2(start0, start1 int) int {
 	var w0, w1 int
 	for n, cnt := range nums {
 		s := state{
 			player: 0,
 			dice:   n,
-			score0: 0,
-			score1: 0,
-			loc0:   start0,
-			loc1:   start1,
+			score:  [2]int{0, 0},
+			loc:    [2]int{start0, start1},
 		}
 		res := play(s)
-		w0 += res.win0 * cnt
-		w1 += res.win1 * cnt
+		w0 += res[0] * cnt
+		w1 += res[1] * cnt
 	}
 
-	fmt.Println("-----------------")
-	fmt.Println("0: ", w0)
-	fmt.Println("1: ", w1)
-	fmt.Println("-----------------")
+	if w0 > w1 {
+		return w0
+	}
+	return w1
 }
 
 func getNums() map[int]int {
@@ -102,10 +100,9 @@ func getNums() map[int]int {
 
 func main() {
 	nums = getNums()
-	cache = make(map[state]win)
+	cache = make(map[state][2]int)
 
-	fmt.Println(nums)
-	part2(4, 8)
-
-	fmt.Println("21: 444356092776315")
+	result := part2(4, 8)
+	fmt.Println("PART02: ", result)
+	fmt.Println(444356092776315 == result)
 }
